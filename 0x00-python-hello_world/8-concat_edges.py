4a35a5fe5bd3ed3e118d205a2537a7e6d0ff7917
MohamedH1000 <mohammedhisham115@gmail.com>
2023-05-08 18:15:41 -0700

    concatenates string with beauty tools

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ we need to understand this language with each other"
+str = str[39:67] + str[-15:-10] + str[:6]
+print(str)
