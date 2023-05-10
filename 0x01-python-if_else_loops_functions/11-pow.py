8cf73e7649db21ffffc577246d39436efd20e261
MohamedH1000 <mohammedhisham115@gmail.com>
2023-05-09 19:43:46 -0700

    some fixations

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def pow(a, b):
-	r = 1
-	n = 0
-	bs = 1
+    r = 1
+    n = 0
+    bs = 1
 
-	if b < 0:
-		n = b
-		b = (-1) * b
+    if b < 0:
+        n = b
+        b = (-1) * b
 
-	for x in range(b):
-		r *= a
-		bs = r * r
+    for x in range(b):
+        r *= a
+        bs = r * r
 
-	if n  < 0:
-		r /= bs
-	return r
+    if n < 0:
+        r /= bs
+    return r
