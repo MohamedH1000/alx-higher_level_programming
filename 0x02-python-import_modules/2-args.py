3445d72ac3a34edeea36be82483475dfbab741f0
MohamedH1000 <mohammedhisham115@gmail.com>
2023-05-11 16:19:35 -0700

    some fixations

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     if an == 0:
         print("{:d} arguments.".format(an))
     elif an == 1:
-        print("{:d} argument.".format(an))
+        print("{:d} argument:".format(an))
         print("{:d}: {:s}".format(a, sys.argv[1]))
     else:
-        print("{:d} arguments.".format(an))
+        print("{:d} arguments:".format(an))
         while a <= an:
             print("{:d}: {:s}".format(a, sys.argv[a]))
             a += 1
