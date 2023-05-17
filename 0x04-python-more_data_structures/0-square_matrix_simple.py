b0390c5a6898ca4180a48cb7668f24ceb43f6d60
MohamedH1000 <mohammedhisham115@gmail.com>
2023-05-17 12:21:55 -0400

    some fixations

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	square = []
-	for row in matrix:
-		b = []
-		for a in row:
-			b.append(a ** 2)
-		square.append(b)
-	return square
+    square = []
+    for row in matrix:
+        b = []
+        for a in row:
+            b.append(a ** 2)
+        square.append(b)
+    return square
