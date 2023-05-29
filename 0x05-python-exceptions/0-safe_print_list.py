70b20c6078b78e60f063f808d0b03dd1d3629305
MohamedH1000 <mohammedhisham115@gmail.com>
2023-05-29 18:50:22 -0400

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     a = 0
     try:
         while a < x:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[a]), end="")
             a += 1
     except Exception:
         pass
