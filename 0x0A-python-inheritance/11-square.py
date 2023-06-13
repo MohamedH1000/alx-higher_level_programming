43bfe58f22b5a5d1951146f0d3c3041ca4efe0f4
MohamedH1000 <mohammedhisham115@gmail.com>
2023-06-12 19:44:50 -0700

    alx

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,5 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """a method that is called print"""
-        return "[{}] {}/{}"
-        /.format(__class__.__name__, self.__size, self.__size)
+        return "[{}] {}/{}".format(__class__.__name__, self.__size, self.__size)
