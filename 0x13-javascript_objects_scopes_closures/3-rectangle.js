6d5ec830547f070908898968f1cc5adf8bdf9faf
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-15 14:57:35 -0400

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+print () {
+  for (let a = 0; a < this.height; a++) {
+    console.log('X'.repeat(this.width));
+  }
+}
+module.exports = Rectangle;
