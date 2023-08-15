3d229330c2371b6a48940db3997f33212f43bc30
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-15 16:56:43 -0400

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let handler = this.width;
+    this.width = this.height;
+    this.height = handler;
+  }
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
