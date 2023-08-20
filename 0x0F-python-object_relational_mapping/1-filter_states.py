dac9a89d68c79e0fbd1888bf4c16d9e9b8ec7d6f
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-20 16:39:04 -0700

    object

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Write a script that lists all states with a name starting
+ with N (upper N) from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dtbs = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], dtbs=sys.argv[3])
-    c = dtbs.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM `state` ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
