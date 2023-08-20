382d1768af321e55a3e5f62c09347ddb18c49ba6
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-20 16:52:16 -0700

    object relational mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+""" write a script that takes in arguments and displays all values"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dtbs = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], dtbs=sys.argv[3])
-    c = dtbs.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
