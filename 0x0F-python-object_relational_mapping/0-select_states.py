ed74a5679f94b0d63aa0d876b4551549482a47a7
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-20 16:35:14 -0700

    object relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""a Write a script that lists all states from the database hbtn_0e_0_usa"""
-"""  to use : ./0-select_states.py <mysql username>"""
-"""                       <mysql password>"""
-"""                       <database name>"""  
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dtbs = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], dtbs=sys.argv[3])
-    c = dtbs.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]
