1e1eb557e37a41320f84a324f1bb53beabb84468
MohamedH1000 <mohammedhisham115@gmail.com>
2023-08-20 16:53:55 -0700

    object relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Write a script that lists all cities from the database """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    dtbs = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], dtbs=sys.argv[3])
-    c = dtbs.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
