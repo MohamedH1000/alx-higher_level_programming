45db9e3fee5ff1ab71280c7f8d496b744951817b
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-27 05:02:47 +0300

    starwars character

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const req = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+req(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
+  for (const c of characters) {
+    request(c, function (error, response, body) {
+      if (error) {
+        console.error('error:', error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
