f48d42d261c12fad74a5dca2dc34ef7b2c4c3d3c
MohamedH1000 <mohammedhisham115@gmail.com>
2023-09-27 05:14:48 +0300

    starwars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
+
 const req = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
+function helpRequest (array, a) {
+  if (a === array.length) {
+    return;
+  }
+  req(array[a], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(JSON.parse(body).name);
+    helpRequest(array, a + 1);
+  });
+}
+
 req(url, function (error, response, body) {
   if (error) {
-    console.error('error:', error);
+    console.error(error);
   }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  for (const c of characters) {
-    req(c, function (error, response, body) {
-      if (error) {
-        console.error('error:', error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  }
+  const ch = JSON.parse(body).characters;
+  helpRequest(ch, 0);
 });
